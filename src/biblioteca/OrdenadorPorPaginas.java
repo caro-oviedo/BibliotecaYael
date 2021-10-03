a44c8402246218c5a0d85dc29ac3b163c92c1591
caro-oviedo <carolina.u.oviedo@gmail.com>
2021-10-03 20:56:02 -0300

    complete project

---
+++ src/biblioteca/OrdenadorPorPaginas.java
@@ -0,0 +1,13 @@
+package biblioteca;
+
+
+import java.util.Comparator;
+
+public class OrdenadorPorPaginas implements Comparator<Libro> {
+
+	@Override
+	public int compare(Libro libro, Libro otroLibro) {
+		return Integer.compare(libro.getPaginas(), otroLibro.getPaginas()) * -1;
+	}
+
+}
